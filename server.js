8ecca478684b71b7ee5e2f0406a83a3c50c3959c
Mayhoon <fell2ratte@t-online.de>
2020-09-03 20:37:50 +0200

    Working

---
+++ server.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var express = require('express');
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static(__dirname + '/scripts'));
+app.use(express.static(__dirname + '/backgrounds'));
+app.use(express.static(__dirname + '/models'));
+app.use(express.static(__dirname + '/three'));
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
