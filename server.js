1b2cb3442eb08f73d9aac0c5f08aa9e683b59f32
Mayhoon <fell2ratte@t-online.de>
2020-09-10 19:40:27 +0200

    New icons

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.get('/', (req, res) => {
 app.use(express.static(__dirname + '/scripts'));
 app.use(express.static(__dirname + '/backgrounds'));
 app.use(express.static(__dirname + '/models'));
-app.use(express.static(__dirname + '/three'));
+app.use(express.static(__dirname + '/fonts'));
+app.use(express.static(__dirname + '/icons'));
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
